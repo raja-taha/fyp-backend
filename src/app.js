8ddd1b12a8457ab0881c9473312410a3849dc975
rajataha-cortechsols <rajataha.cortechsols@gmail.com>
2025-03-04 12:05:55 +0500

    CORS handled

--- src/app.js
+++ src/app.js
@@ -17,7 +17,14 @@ const app = express();
 
 // Middleware setup
 app.use(express.json()); // Parse JSON requests
-app.use(cors()); // Enable CORS for all origins
+app.use(
+  cors({
+    origin: "*", // Change this to your frontend domain instead of "*"
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+); // Enable CORS for all origins
 app.use(morgan("dev")); // Log HTTP requests
 app.use("/src/public", express.static(path.join(__dirname, "src/public")));
 
