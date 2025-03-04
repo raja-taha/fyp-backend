29eeaa2dd69484f373d4053e5656e3a8b32c3a0e
Muhammad Taha <98023470+raja-taha@users.noreply.github.com>
2025-03-04 17:53:45 +0500

    Update logger.js

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,24 +1,24 @@
-const winston = require("winston");
-const fs = require("fs");
-const path = require("path");
+// const winston = require("winston");
+// const fs = require("fs");
+// const path = require("path");
 
-// Ensure the logs directory exists
-const logDir = "logs";
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
-}
+// // Ensure the logs directory exists
+// const logDir = "logs";
+// if (!fs.existsSync(logDir)) {
+//   fs.mkdirSync(logDir);
+// }
 
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-    })
-  ),
-  transports: [
-    new winston.transports.File({ filename: path.join(logDir, "app.log") }), // Log to file
-  ],
-});
+// const logger = winston.createLogger({
+//   level: "info",
+//   format: winston.format.combine(
+//     winston.format.timestamp(),
+//     winston.format.printf(({ timestamp, level, message }) => {
+//       return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+//     })
+//   ),
+//   transports: [
+//     new winston.transports.File({ filename: path.join(logDir, "app.log") }), // Log to file
+//   ],
+// });
 
-module.exports = logger;
+// module.exports = logger;
